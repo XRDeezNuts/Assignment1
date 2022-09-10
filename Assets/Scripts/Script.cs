e9ed9fed31dd1d0095d64843405df860b31cf976
weilderbeast <bogdan.is.a.beast@gmail.com>
2022-09-10 14:36:50 +0200

    commiting whole project due to git lfs not working on existing commits (group work)

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class MyPrefabInstantiator : DefaultObserverEventHandler
+{
+        public GameObject modelPrefab;
+        private GameObject _modelObject;
+
+        protected override void OnTrackingFound()
+        {
+                Debug.Log("tracking found");
+                // Instantiate the model prefab only if it hasn't been instantiated yet
+                if (_modelObject == null) 
+                        InstantiatePrefab();
+                
+                base.OnTrackingFound();
+        }
+
+        private void InstantiatePrefab()
+        {
+                if (modelPrefab == null) return;
+                Debug.Log("Target found, adding content"); 
+                _modelObject = Instantiate(modelPrefab, mObserverBehaviour.transform);
+                _modelObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                _modelObject.SetActive(true);
+        }
+}
